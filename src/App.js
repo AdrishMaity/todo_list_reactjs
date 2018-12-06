da34dd096deb711c27262ea409b3f2271138e8a0
Adrish <adrish.maity@outlook.com>
2018-12-07 00:18:20 +0530

    Basic Todo list app developed, it displays hardcoded todo list and has a option to delete upon clicking on the item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos'
+
 
 class App extends Component {
+  state = {
+    todos : [
+      {id:1, content: 'buy some rice'},
+      {id:2, content: 'read react'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    let todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos: todos
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo }/>
       </div>
     );
   }
